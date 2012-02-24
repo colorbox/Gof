a48efce179a7409eff2e083e7377bc8dce9bf2e0
colorbox <colorbox222@gmail.com>
2012-02-24 12:05:16 +0900

    FactoryMethod

--- FactoryMethod/idcard/IDCard.java
+++ FactoryMethod/idcard/IDCard.java
@@ -3,14 +3,19 @@ import framework.*;
 
 public class IDCard extends Product{
     private String owner ;
-    IDCard(String owner){
+    private int Booknumber;
+    IDCard(String owner,int Booknumber){
 	System.out.println(owner+"のカードを作ります");
 	this.owner=owner;
+	this.Booknumber=Booknumber;
     }
     public void use(){
-	System.out.println(owner+"のカードを使います");
+	System.out.println(owner+"のカード"+Booknumber+"番を使います");
     }
     public String getOwner(){
 	return owner;
+    }
+    public int getNumber(){
+	return Booknumber;
     }
 }
