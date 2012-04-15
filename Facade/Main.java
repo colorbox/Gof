0670f190357d112c1e969376ac82d5975d885534
colorbox <colorbox222@gmail.com>
2012-04-15 21:37:23 +0900

    Facade with solved Problem

---
+++ Facade/Main.java
@@ -0,0 +1,8 @@
+import pagemaker.PageMaker;
+
+public class Main{
+    public static void main(String[] args){
+	//	PageMaker.makeWelcomePage("hyukihyuki@hyuki.com","welcome.html");
+	PageMaker.makeLinkPage("test.html");
+    }
+}
