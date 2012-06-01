5ded9ede94b8b4ba6f0196ecd691fab47453994f
colorbox <colorbox222@gmail.com>
2012-06-02 00:32:37 +0900

    Iterator
    created Book,Aggregate

---
+++ objective-c/Iterator/Iterator/Iterator/Aggregate.h
@@ -0,0 +1,13 @@
+//
+//  Aggregate.h
+//  Iterator
+//
+//  Created by 健太郎 眞嶋 on 12/06/01.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol Aggregate <NSObject>
+-(Iterator)iterator;
+@end
